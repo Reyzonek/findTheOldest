a888cfd90744a8854c0eba1eb12ce3eb69d92465
reyzonek <pawel1221@gmail.com>
2021-12-27 13:19:16 +0100

    with only one for loop

--- javaScript.js
+++ javaScript.js
@@ -18,15 +18,15 @@ const metryka = [
 
 function findTheOldest(){
   let tablicaWieku = [];
+  let oldest = 0;
   for (let i = 0; i < metryka.length; i++){
+    
     let wiek = metryka[i].death - metryka[i].born;
     tablicaWieku.push(wiek);
-  }
-  let oldest = 0;
-  for (let j = 0; j < tablicaWieku.length; j++){    
-    if (tablicaWieku[j] > oldest){
-      oldest = tablicaWieku[j];
-      wiekNumber = j;     
+      
+    if (tablicaWieku[i] > oldest){
+      oldest = tablicaWieku[i];
+      wiekNumber = i;     
     }
   }
 return metryka[wiekNumber];  
